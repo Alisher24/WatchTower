d59be4292f3138062325f189c51aeb64179b84f9
Alisher <baiyskulov.alisher@gmail.com>
2024-06-28 12:06:54 +0700

    beta version start

---
+++ WatchTowerBeta/WatchTowerBeta/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WatchTowerBeta.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
